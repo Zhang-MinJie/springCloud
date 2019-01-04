c7e77abfc637db434f71fc6696622aa22e850fef
zhangminjie <zhangminjie@minshenglife.com>
2019-01-04 15:34:48 +0800

    add Spring Cloud OpenFeign

---
+++ eurekaClientConsumer/src/main/java/cn/zhmj/example/sprinCloud/eurekaClientConsumer/api/EurekaClientProducerApi.java
@@ -0,0 +1,11 @@
+package cn.zhmj.example.sprinCloud.eurekaClientConsumer.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("eurekaClientProducer")
+public interface EurekaClientProducerApi {
+	@RequestMapping(method = RequestMethod.GET, value = "/hellow")
+    public String hellow();
+}
